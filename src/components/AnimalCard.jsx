a6c829107c156ecd4e18d84c6ca386f1d227c63d
Nina Simmons <pkpbabies@gmail.com>
2025-02-07 19:11:39 -0500

    Add Animal component

--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -1,1 +1,24 @@
 
+// Our local shelter needs identification cards for all the animals ready to be adopted. We volunteer your time to help with some of those.
+
+// Write a new component called AnimalCard that accepts 3 props ```title, description, and imageUrl.```
+
+// Inside the component, render the title in an `<h1>` tag, the description in a `<p>` tag, and pass the `imageUrl` into an img tag like `<img src={imageUrl}/>`. Make five instances of this component with different props (data).
+
+// #### Note: You can use any image that you want. The title should be a name, and the description should be a little about the animal. 
+
+// #### Bonus: Use `alt` text with props for your images.
+
+import React from 'react';
+
+const AnimalCard = ({title, description, imageUrl, altText }) => {
+  return (
+    <div className ='animal-card'>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <img src= {imageUrl} alt={altText} />
+    </div>
+  )
+}
+
+export default AnimalCard;
